c4c110acc72cda9949a63d656d62d073b72828a1
Crispin Wellington <retrogradeorbit@gmail.com>
2026-04-24 23:42:45 +0800

    docker test utils

---
+++ test/cljssh_test/docker.clj
@@ -0,0 +1,85 @@
+(ns clj-test.docker
+  (:require [babashka.pods :as pods]
+            [babashka.process :as process]
+            [babashka.wait :as wait]
+            [clojure.string :as string])
+  (:import [java.lang.ref WeakReference]))
+
+(defn run [command error-message]
+  (let [{:keys [exit err out]}
+        (process/sh command)]
+    (assert (zero? exit) (str error-message ": out:" out " err:" err))
+    out))
+
+(defn run! [command]
+  (process/sh command))
+
+(defn build [{:keys [root-password]}]
+  (run
+    (format
+     "docker build -t cljssh/test-base --build-arg root_password=%s test"
+     root-password)
+    "docker build failed"))
+
+(defn cleanup []
+  (run! "docker container stop cljssh-test")
+  (run! "docker container rm cljssh-test")
+  nil)
+
+(defn start [{:keys [ssh-port]}]
+  (-> "docker run --name cljssh-test -d -p %d:22 cljssh/test-base"
+      (format ssh-port)
+      (run "docker run failed")
+      string/trim))
+
+(defn stop []
+  (run! "docker container stop cljssh-test"))
+
+(defn exec [command]
+  (run
+    (str "docker exec cljssh-test " command)
+    "docker exec failed"))
+
+(defn exec! [command]
+  (run!
+    (str "docker exec cljssh-test " command)))
+
+(defn cp-to [local-src remote-dest]
+  (run
+    (format "docker cp \"%s\" \"cljssh-test:%s\"" local-src remote-dest)
+    "docker cp failed"))
+
+(defn cp-from [remote-src local-dest]
+  (run
+    (format "docker cp \"cljssh-test:%s\" \"%s\"" remote-src local-dest)
+    "docker cp failed"))
+
+(defn put-file [contents remote-dest]
+  (process/sh
+   ["docker" "exec" "cljssh-test" "ash" "-c"
+    (format "echo '%s' > '%s'"
+            contents
+            remote-dest)]))
+
+(defn put-dir
+  "transfer a complete local directory to the docker container"
+  [src-dir src-path dest-path]
+  (process/sh "rm /tmp/cljssh-tarball.tgz")
+  (process/sh (format "tar -cvz -C '%s' -f /tmp/cljssh-tarball.tgz '%s'" src-dir src-path))
+  (exec "rm -f /tmp/cljssh-tarball.tgz")
+  (cp-to "/tmp/cljssh-tarball.tgz" "/tmp/cljssh-tarball.tgz")
+  (exec
+   (format "tar -xv -f /tmp/cljssh-tarball.tgz -C '%s'"
+           dest-path)))
+
+(defn md5 [path]
+  (-> (exec (format "md5sum '%s'" path))
+      (string/split #" ")
+      first))
+
+(defn get-container-ip
+  []
+  (-> (process/sh
+        "docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cljssh-test")
+      :out
+      (string/trim)))
